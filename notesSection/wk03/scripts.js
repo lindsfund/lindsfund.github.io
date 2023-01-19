b39a414bc66fc47a31f6a616e6aeeb0d54d3409b
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-01-19 10:54:15 -0500

    web API notes

---
+++ notesSection/wk03/scripts.js
@@ -0,0 +1,68 @@
+// Create an AudioContext (cross browser) API ENTRANCE
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+ 
+// store references to our HTML elements
+const audioElement = document.querySelector('audio');
+const playBtn = document.querySelector('button');
+const volumeSlider = document.querySelector('.volume');
+ 
+// load the audio source into our audio graph
+const audioSource = audioCtx.createMediaElementSource(audioElement);
+ 
+// play/pause audio
+playBtn.addEventListener('click', () => {
+    // check if context is in suspended state (autoplay policy)
+    if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
+    }
+ 
+  // if track is stopped, play it
+    if (playBtn.getAttribute('class') === 'paused') {
+        audioElement.play();
+        playBtn.setAttribute('class', 'playing');
+        playBtn.textContent = 'Pause'
+    // if track is playing, stop it
+} else if (playBtn.getAttribute('class') === 'playing') {
+        audioElement.pause();
+        playBtn.setAttribute('class', 'paused');
+        playBtn.textContent = 'Play';
+    }
+});
+
+// if track ends
+audioElement.addEventListener('ended', () => {
+    playBtn.setAttribute('class', 'paused');
+    playBtn.textContent = 'Play'
+});
+
+// volume
+const gainNode = audioCtx.createGain();
+
+volumeSlider.addEventListener('input', () => {
+    gainNode.gain.value = volumeSlider.value;
+});
+
+// connect our graph
+audioSource.connect(gainNode).connect(audioCtx.destination);
+
+// Track credit: Outfoxing the Fox by Kevin MacLeod under Creative Commons
+
+//DOM API
+const em = document.createElement('em');
+const showEM = document.getElementById("domAPI");
+em.textContent = '.............. Hi there!';
+showEM.appendChild(em);
+
+//CANVAS API
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+ ctx.beginPath();
+ ctx.moveTo(50, 150);
+ ctx.lineTo(50, 50);
+ ctx.lineTo(150, 50);
+ ctx.lineTo(150,150);
+ ctx.closePath();
+ ctx.stroke();
+ ctx.fillRect(80,80,40,40);
