d5935d365c36fa13b72ad1cf19952f961ed43350
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-02-02 13:28:39 -0500

    notes for Thursday

--- notesSection/wk05/scripts.js
+++ notesSection/wk05/scripts.js
@@ -57,15 +57,82 @@ headers();
 
 //-----------------------------------------
 
-let urlMine = './user.json';
-async function tryFetch(){
-    let response = await fetch(urlMine);
-    console.log(response);
-    let data = await response.json();
-    console.log(data);
+function outputToPage(obj){
+    const location = document.getElementById('aHeader');
+    const para = document.createElement('p');
 
-  let key = response.headers.entries();
-  console.log(key);
+    para.textContent = `First Name: ${obj.firstName} Last Name: ${obj.lastName}`;
+    location.appendChild(para);
 
 }
-tryFetch();
+
+
+// get the info from the JSON
+async function getJsonInfo() {
+    //request json file
+    const urlMine = './user.json';
+    const request = new Request(urlMine);
+    //console.log(request);
+    
+    //read json file or give error
+    const response = await fetch(request);
+    if(!response.ok){
+        const message = `An error ahs occured: ${response.status}`;
+        throw new Error(message);
+    }
+    //console.log(response);
+
+    //display info in json file
+    const info = await response.json();
+    //console.log(info.users[0]);
+   
+    // PARSE JSON -- not in this case
+        // const parsedInfo = JSON.parse(info);
+        // console.log(parsedInfo.firstName);
+
+    // STRINGIFY JSON
+       // const stringInfo = JSON.stringify(info);
+       // console.log(stringInfo);
+   
+       
+   
+    //loop through array of users - OLD SCHOOL
+        //    for(i = 0; i < info.users.length; i++) {
+        //     console.log(info.users[i]);
+        //    }
+
+    //loop through using forEach - NEW SCHOOL
+   info.users.forEach(function (user) {
+    outputToPage(user);
+   })
+
+   return info;
+}
+
+
+getJsonInfo();
+
+const newUser = {
+    firstName: 'Bob',
+    lastName: 'Smith'
+};
+
+async function addJsonInfo() {
+    const urlMine = './user.json';
+    const request = new Request(urlMine, {
+        method:'POST',
+        body:JSON.stringify(newUser)
+    });
+    console.log(request.method);
+    console.log(newUser);
+
+    let response = await fetch(urlMine);
+    console.log(response);
+
+    let result = await response.json();
+    console.log(result);
+
+}
+
+
+addJsonInfo();
