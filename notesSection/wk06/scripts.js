443b544165c49b9e61f173d221f8902a6bcfc4b1
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-02-09 12:35:56 -0500

    notes for basic forms

--- notesSection/wk06/scripts.js
+++ notesSection/wk06/scripts.js
@@ -22,3 +22,76 @@ let imageGalleryTemplate = function imageGalleryTemplate (imgs) {
 document.getElementById('wkNumHead').textContent += "Week 06 Notes";
 
 // scripts to enter notes into the DOM
+function notesToHtml(obj, id, elemType, note, cls) {
+  let insHere = document.getElementById(`${id}`);
+  let elem = document.createElement(`${elemType}`);
+  //set default to none and make it so that class is optional 
+  if (typeof(cls) != 'undefined') {
+      this.cls = cls;
+      elem.className = cls;
+  }
+  //set note to optional put in before if it it there and leave it out if it's not
+  if (typeof(note) != 'undefined') {
+    this.note = note;
+    elem.innerHTML = note + obj;
+  } else {
+    elem.innerHTML = obj;
+  }
+  
+
+  insHere.append(elem);
+}
+
+//----------------------------------------------------------Notes
+//get the whole form
+let form = document.forms.my;
+
+notesToHtml(form,'mainInsert', 'p', '(see console for HTML notes) Get the whole form: ');
+console.log(form);
+
+//get the element
+let elem = form.elements.one;
+console.log(elem);
+
+//elements with the same name
+let sameElemName = form.elements.age;
+console.log(sameElemName); //this prints a node list
+console.log(sameElemName[0]);
+console.log(sameElemName[1]);// these return the individual elements
+
+let fieldSet = form.elements.login;
+console.log(fieldSet); 
+
+notesToHtml(my.login == form.login, 'mainInsert', 'p', 'my.login == form.login: '); //true
+
+//accessing values
+notesToHtml(sameElemName[0].value, 'mainInsert', 'p','Values: '); // .value acces the value instead of the HTML
+notesToHtml(sameElemName[1].value, 'mainInsert', 'p','Values: ');
+notesToHtml(sameElemName[0].checked, 'mainInsert', 'p','Values: ');//.checked will return true or false depending on weather the radio button or box is filled.
+
+//select and option
+select.options[2].selected = true; //options are zero indexed like arrays so the third is 2
+select.selectedIndex = 1;
+select.value = 'banana' 
+
+//get all that are selected when multiple are allowed
+let selected = Array.from(selectMult.options)
+                    .filter(option => option.selected)
+                    .map(option => option.value);
+
+notesToHtml(selected, "mainInsert", 'p', 'Selected from music options: ', 'mkItGreen');
+
+//add new (unselected)option to the list ** to add a selected property add ' ,true ,true' at the end
+let option = new Option('text', 'value',true,true);//creates <option value='value'> TEXT </option>
+console.log(option);
+
+
+//------------------------FORMS SUBMIT
+
+// function alertFunction(input) {
+//   alert('You clicked submit.');
+// }
+
+
+
+
