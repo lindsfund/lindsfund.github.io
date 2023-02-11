f44d52a5b040f05f1c48bc7f9832c1f8627f8bed
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-02-11 15:42:44 -0500

    finished notes for week 06

--- notesSection/wk06/scripts.js
+++ notesSection/wk06/scripts.js
@@ -89,17 +89,16 @@ console.log(option);
 //------------------------FORMDATA Objects
 //event listener
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('formObject').addEventListener('submit', handleForm);
+  document
+    .getElementById('myForm')
+    .addEventListener('submit', handleForm);
 });
 
-//function to handle info inside the form
-
 function handleForm(ev) {
-  ev.preventDefault(); //This wioll stop the page from relaoding *** VERY IMPORTANT***
-  console.log(ev.target);
-
+  ev.preventDefault(); //stop the page reloading
+  //console.dir(ev.target);
   let myForm = ev.target;
-  let fd = new FormData(formObject);
+  let fd = new FormData(myForm);
 
   //add more things that were not in the form
   fd.append('api-key', 'myApiKey');
@@ -108,11 +107,35 @@ function handleForm(ev) {
   for (let key of fd.keys()) {
     console.log(key, fd.get(key));
   }
+  let json = convertFD2JSON(fd);
 
   //send the request with the formdata
-  
-} 
+  let url = 'http://localhost:3000/';
+  let h = new Headers();
+  h.append('Content-type', 'application/json');
 
+  let req = new Request(url, {
+    headers: h,
+    body: json,
+    method: 'POST',
+  });
+  //console.log(req);
+  fetch(req)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Response from server');
+      console.log(data);
+    })
+    .catch(console.warn);
+}
+
+function convertFD2JSON(formData) {
+  let obj = {};
+  for (let key of formData.keys()) {
+    obj[key] = formData.get(key);
+  }
+  return JSON.stringify(obj);
+}
 
 
 
