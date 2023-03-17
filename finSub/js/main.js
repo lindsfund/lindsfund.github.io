71e19d11d51446db831f72265d853eb09fb4de85
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-03-17 13:50:48 -0400

    update info from main.js

--- finSub/js/main.js
+++ finSub/js/main.js
@@ -4,31 +4,26 @@ import WeatherData from "./weatherInfo.mjs";
 import Seeds from "./fromJson.mjs";
 import {pickStation } from "./apiReturns.mjs";
 
-
-
-
-
-
-
-
-
 loadHeaderFooter();
-const seeds = new Seeds();
 
 
 //get location info
 // const station = await pickStation();
 //         console.log(station);
 
-//get frostDate
+
+
+// get frostDate
 const frostDate = await getFrostDate();
-        console.log(frostDate);
+        console.log(`80% chance of frost on ${frostDate}`);
 
 //get weather data
 const weather = new WeatherData();
 const finWeather = weather.init(weather);
         console.log(finWeather);
 
+//get seed info
+const seeds = new Seeds();
 
 
 
