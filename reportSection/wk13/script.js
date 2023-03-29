18931ba7f3478699ed2a9360420bfc9e8aa9b634
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-03-29 19:11:42 -0400

    final report

--- reportSection/wk13/script.js
+++ reportSection/wk13/script.js
@@ -16,11 +16,14 @@ function reportTemplate() {
         </div>`;
   }
 
-const q1 = `Lorem Ipsum`;
+const q1 = `This last week I finished up the functionality on my garden page. I also cleaned 
+up the code and made the site more presentable with CSS. I also recorded the presentation 
+for the final project and completed a text document that contained all the necessary links 
+for submission. Finally, I submitted all that in the week 14 final submission section.`;
 
-const q2 = `Lorem Ipsum`;
+const q2 = `Next I will wait for the grades to be posted and/or questions on my submission.`;
 
-const q3 = `Lorem Ipsum`;
+const q3 = `There were no blockers this week. I was able to accomplish everything I set out to do.`;
 
 function renderReportTemplate(selector) {
   const element = document.querySelector(selector);
